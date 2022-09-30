405b2ae67f521cc61801b52e0458e62d8725ddce
Oghenero Ologe <nologe37@gmail.com>
2022-09-30 15:27:04 +0100

    passing test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -17,7 +17,7 @@ it("responds with null current user if not authenticated", async () => {
   const res = await request(app)
     .get("/api/users/currentuser")
     .send()
-    .expect(400);
+    .expect(200);
   // console.log(res.body);
   expect(res.body.user).toEqual(null);
 });
