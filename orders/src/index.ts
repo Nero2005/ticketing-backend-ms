f337ffef8a056a38f224c61a9686e3ae7e8bf62c
Oghenero Ologe <nologe37@gmail.com>
2022-09-30 15:48:06 +0100

    testing selective tests

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,7 @@ import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listene
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async (app: Application) => {
+  console.log("Starting up...");
   if (!process.env.JWT_KEY) {
     throw new Error("No JWT_KEY");
   }
